1cc309e49830cf1845fc0df9f2ed40810a87ae30
ahmetgokdemir <ahmetgokdemirtc@gmail.com>
2022-10-07 17:25:53 +0300

    BeginingOfRepository

---
+++ Project.DAL/Repositories/Abstracts/IRepository.cs
@@ -0,0 +1,68 @@
+﻿using Project.ENTITIES.CoreInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Repositories.Abstracts
+{
+
+    public interface IRepository<T> where T : class, IEntity
+    {
+        // Task AddAsync(T item) gibi yorum satırına alınan crud işlemler Token dersinden
+
+        // List Commands
+        // List<T> GetAll(); // IQueryable<T> GetAllAsync();
+        Task<IEnumerable<T>> GetAllAsync();
+
+        //List<T> GetActives();
+        Task<IQueryable<T>> GetActivesAsync();
+        // List<T> GetPassives();
+        Task<IQueryable<T>> GetPassivesAsync();
+
+        // List<T> GetModifieds();
+        Task<IQueryable<T>> GetModifiedsAsync();
+
+
+        //Modify Commands
+        //void Add(T item);
+        Task AddAsync(T entity);
+        // void AddRange(List<T> list);
+        Task AddRangeAsync(List<T> list);
+
+        // void Delete(T item);
+        void Remove(T entity);
+        void RemoveRange(List<T> list);
+
+        void Destroy(T item);
+        void DestroyRange(List<T> list);
+
+        void Update(T item); // T Update(T entity);
+        void UpdateRange(List<T> list);
+
+        //Linq
+        // List<T> Where(Expression<Func<T, bool>> exp);  
+        IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+
+        bool Any(Expression<Func<T, bool>> exp);
+        T FirstOrDefault(Expression<Func<T, bool>> exp);
+        object Select(Expression<Func<T, object>> exp);
+        object SelectViaClass<X>(Expression<Func<T, X>> exp);
+
+        //Find Command
+        // T Find(int id); // Task<T> GetByIdAsync(int id);
+        Task<T> GetByIdAsync(int id);
+
+        //Last Data
+        // T GetLastData();
+        Task<T> GetLastDataAsync();
+
+        //First Data
+        // T GetFirstData();
+        Task<T> GetFirstDataAsync();
+
+    }
+
+}
---
+++ Project.DAL/Repositories/Concretes/BaseRepository.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.DAL.Context;
+using Project.DAL.Repositories.Abstracts;
+using Project.ENTITIES.CoreInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Repositories.Concretes
+{
+    public class BaseRepository<T> : IRepository<T> where T : class, IEntity
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<IEntity> _dbSet;
+
+        public BaseRepository(SemerkandDergilikContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<IEntity>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public Task AddRangeAsync(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Any(Expression<Func<T, bool>> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Destroy(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DestroyRange(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FirstOrDefault(Expression<Func<T, bool>> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<T>> GetActivesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            //return _context.Set<T>().ToListAsync();
+            return (IEnumerable<T>)await _dbSet.ToListAsync();
+            //throw new NotImplementedException();
+
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetFirstDataAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetLastDataAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<T>> GetModifiedsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<T>> GetPassivesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Select(Expression<Func<T, object>> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object SelectViaClass<X>(Expression<Func<T, X>> exp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRange(List<T> list)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
