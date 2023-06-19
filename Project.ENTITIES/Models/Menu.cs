a01090eec546899d492ec73aabc60c90ef9151ce
ahmetgokdemir <ahmetgokdemirtc@gmail.com>
2023-06-19 17:01:39 +0300

    Menu Tablosu

---
+++ Project.ENTITIES/Models/Menu.cs
@@ -0,0 +1,28 @@
+﻿using Project.ENTITIES.CoreInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.ENTITIES.Models
+{
+    public class Menu : EntityBase, IEntity
+    {
+        public string Menu_Name { get; set; }
+
+        // int kategory_id olabilir
+        public Dictionary<int, Food> _foodItems { get; set; }
+        public decimal? Menu_Price
+        {
+            get
+            {
+                return _foodItems.Sum(x => x.Value.UnitPrice); 
+
+            }
+
+            // list = dsfd for 
+        }
+
+    }
+}
